696163aadd5d95527a6c49db5bc4cfa46addd5cd
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2018-04-03 03:52:19 +0000

    Fix d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,11 @@
-declare module KarmiaUtility {
-    export class KarmiaUtilityObject {
-        isObject(value: any): boolean;
-        isPlainObject(value: any): boolean;
-        flip(object: Object): Object;
-        mergeProperties(object1: Object, object2: Object): Object;
-        removeProperty(object: Object, properties: Array<any>|any): Object;
-    }
+declare class KarmiaUtilityObject {
+    constructor();
+    isObject(value: any): boolean;
+    isPlainObject(value: any): boolean;
+    flip(object: object): object;
+    mergeProperties(object1: object, object2: object): object;
+    removeProperty(object: object, properties: Array<any>|any): object;
 }
+
+declare function karmia_utility_object(options: object): KarmiaUtilityObject;
+export = karmia_utility_object;
