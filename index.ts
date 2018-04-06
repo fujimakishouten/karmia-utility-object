908c481946dcad987092fddc758c60fe70922195
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2018-04-06 12:50:30 +0900

    Change to use TypeScript

--- index.ts
+++ index.ts
@@ -2,52 +2,57 @@
 /* eslint-env es6, mocha, node */
 /* eslint-extends: eslint:recommended */
 'use strict';
+
+
 /**
  * KarmiaUtilityObject
  *
  * @class
  */
 class KarmiaUtilityObject {
-    constructor() {
-        /**
-         * Properties
-         */
-        this.isObject = KarmiaUtilityObject.isObject;
-        this.isPlainObject = KarmiaUtilityObject.isPlainObject;
-        this.flip = KarmiaUtilityObject.flip;
-        this.mergeProperties = KarmiaUtilityObject.mergeProperties;
-        this.removeProperty = KarmiaUtilityObject.removeProperty;
-    }
+    /**
+     * Properties
+     */
+    public isObject = KarmiaUtilityObject.isObject;
+    public isPlainObject = KarmiaUtilityObject.isPlainObject;
+    public flip = KarmiaUtilityObject.flip;
+    public mergeProperties = KarmiaUtilityObject.mergeProperties;
+    public removeProperty = KarmiaUtilityObject.removeProperty;
+
     /**
      * Check if value is an instance of Object
      *
      * @param {*} value
      * @returns {boolean}
      */
-    static isObject(value) {
+    static isObject(value: any) {
         return (value instanceof Object);
     }
+
     /**
      * Check if value is a plain object
      *
      * @param {*} value
      * @returns {boolean}
      */
-    static isPlainObject(value) {
+    static isPlainObject(value: any) {
         return (Object.getPrototypeOf(value) === Object.prototype);
     }
+
     /**
      * Get object that exchanges key with their value
      *
      * @param   {Object} object
      * @returns {Object}
      */
-    static flip(object) {
-        return Object.keys(object).reduce(function (collection, key) {
+    static flip(object: {[index: string]: any}) {
+        return Object.keys(object).reduce(function (collection: {[index: string]: any}, key: any) {
             collection[object[key]] = key;
+
             return collection;
         }, {});
     }
+
     /**
      * Recursively merge properties
      *
@@ -55,20 +60,23 @@ class KarmiaUtilityObject {
      * @param {Object} object2
      * @returns {Object}
      */
-    static mergeProperties(object1, object2) {
+    static mergeProperties(object1: {[index: string]: any}, object2: {[index: string]: any}) {
         return Object.keys(object2).reduce(function (collection, key) {
             if (key in collection) {
                 if (Object.getPrototypeOf(collection[key]) === Object.prototype) {
                     if (Object.getPrototypeOf(object2[key]) === Object.prototype) {
                         collection[key] = KarmiaUtilityObject.mergeProperties(collection[key], object2[key]);
+
                         return collection;
                     }
                 }
             }
             collection[key] = object2[key];
+
             return collection;
         }, Object.assign({}, object1));
     }
+
     /**
      * Remove property from object
      *
@@ -76,19 +84,35 @@ class KarmiaUtilityObject {
      * @param   {Array} properties
      * @returns {Object}
      */
-    static removeProperty(object, properties) {
+    static removeProperty(object: {[index: string]: any}, properties: Array<string>|string) {
         properties = properties || [];
         const list = Array.isArray(properties) ? properties : [properties];
-        return Object.keys(object).reduce(function (collection, key) {
+
+        return Object.keys(object).reduce(function (collection: {[index: string]: any}, key: any) {
             if (-1 < list.indexOf(key)) {
                 return collection;
             }
+
             collection[key] = object[key];
             if (KarmiaUtilityObject.isObject(object[key])) {
                 collection[key] = KarmiaUtilityObject.removeProperty(collection[key], list);
             }
+
             return collection;
         }, {});
     }
 }
-module.exports = KarmiaUtilityObject;
+
+
+// Export module
+export = KarmiaUtilityObject;
+
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
--- test/karmia-utility-object.ts
+++ test/karmia-utility-object.ts
@@ -2,10 +2,14 @@
 /* eslint-env es6, mocha, node */
 /* eslint-extends: eslint:recommended */
 'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
+
+
+
 // Import modules
-const KarmiaUtilityObject = require("../");
+import KarmiaUtilityObject = require("../");
 const expect = require("expect.js");
+
+
 // Test
 describe('karmia-utility-object', function () {
     describe('isObject', function () {
@@ -13,114 +17,134 @@ describe('karmia-utility-object', function () {
             it('Object', function () {
                 expect(KarmiaUtilityObject.isObject({})).to.be(true);
             });
+
             it('Array', function () {
                 expect(KarmiaUtilityObject.isObject([])).to.be(true);
             });
+
             it('Function', function () {
-                expect(KarmiaUtilityObject.isObject(function () { })).to.be(true);
+                expect(KarmiaUtilityObject.isObject(function () {})).to.be(true);
             });
+
             it('Generator', function () {
                 expect(KarmiaUtilityObject.isObject(function* () { return true; })).to.be(true);
             });
         });
+
         describe('Should not be object', function () {
             it('string', function () {
                 expect(KarmiaUtilityObject.isObject('a')).to.be(false);
             });
+
             it('number', function () {
                 expect(KarmiaUtilityObject.isObject(1.0)).to.be(false);
             });
+
             it('boolean', function () {
                 expect(KarmiaUtilityObject.isObject(true)).to.be(false);
             });
         });
     });
+
     describe('isPlainObject', function () {
         describe('Should be object', function () {
             it('Object', function () {
                 expect(KarmiaUtilityObject.isPlainObject({})).to.be(true);
             });
         });
+
         describe('Should not be object', function () {
             it('Array', function () {
                 expect(KarmiaUtilityObject.isPlainObject([])).to.be(false);
             });
+
             it('Function', function () {
-                expect(KarmiaUtilityObject.isPlainObject(function () { })).to.be(false);
+                expect(KarmiaUtilityObject.isPlainObject(function () {})).to.be(false);
             });
+
             it('Generator', function () {
                 expect(KarmiaUtilityObject.isPlainObject(function* () { return true; })).to.be(false);
             });
+
             it('string', function () {
                 expect(KarmiaUtilityObject.isPlainObject('a')).to.be(false);
             });
+
             it('number', function () {
                 expect(KarmiaUtilityObject.isPlainObject(1.0)).to.be(false);
             });
+
             it('boolean', function () {
                 expect(KarmiaUtilityObject.isPlainObject(true)).to.be(false);
             });
         });
     });
+
     describe('flip', function () {
         it('Should get object that exchanges key with their value', function () {
-            const object = { a: 'A', b: 'B', c: 'C', d: 1, e: 2, f: 3 }, result = KarmiaUtilityObject.flip(object);
+            const object = {a: 'A', b: 'B', c: 'C', d: 1, e: 2, f: 3} as {[index: string]: number|string},
+                result = KarmiaUtilityObject.flip(object) as {[index: string]: any};
+
             Object.keys(object).forEach(function (key) {
                 expect(result[object[key]]).to.be(key);
             });
         });
     });
+
     describe('mergeProperties', function () {
         it('Should merge property', function () {
             const object1 = {
-                a: 'A',
-                b: {
-                    c: 'C',
-                    d: {
-                        e: 'E',
-                        f: {
-                            g: 'G',
-                            h: {
-                                i: 'I',
-                                j: {
-                                    k: 'K',
-                                    l: {
-                                        m: 'M'
-                                    },
-                                    n: {
-                                        o: 'O'
+                    a: 'A',
+                    b: {
+                        c: 'C',
+                        d: {
+                            e: 'E',
+                            f: {
+                                g: 'G',
+                                h: {
+                                    i: 'I',
+                                    j: {
+                                        k: 'K',
+                                        l: {
+                                            m: 'M'
+                                        },
+                                        n: {
+                                            o: 'O'
+                                        }
                                     }
                                 }
                             }
-                        }
-                    }
-                }
-            }, object2 = {
-                b: {
-                    d: {
-                        f: {
-                            g: 'G-2',
-                            h: {
-                                j: {
-                                    n: 'N',
-                                    o: 'O',
-                                    p: {
-                                        q: 'Q'
-                                    }
-                                }
-                            }
-                        },
-                        r: 'R',
-                        s: {
-                            t: 'T'
                         }
                     }
                 },
-                u: 'U',
-                v: {
-                    w: 'W'
-                }
-            }, result = KarmiaUtilityObject.mergeProperties(object1, object2);
+                object2 = {
+                    b: {
+                        d: {
+                            f: {
+                                g: 'G-2',
+                                h: {
+                                    j: {
+                                        n: 'N',
+                                        o: 'O',
+                                        p: {
+                                            q: 'Q'
+                                        }
+                                    }
+                                }
+                            },
+                            r: 'R',
+                            s: {
+                                t: 'T'
+                            }
+                        }
+                    },
+                    u: 'U',
+                    v: {
+                        w: 'W'
+                    }
+                },
+                result = KarmiaUtilityObject.mergeProperties(object1, object2);
+
             expect(result).to.eql({
                 a: 'A',
                 b: {
@@ -157,27 +181,33 @@ describe('karmia-utility-object', function () {
             });
         });
     });
+
     describe('removeProperty', function () {
         describe('Should remove property', function () {
             it('Should remove top level property', function () {
-                const object = { a: 'A', b: 'B', c: 'C', d: 1, e: 2, f: 3 }, result = KarmiaUtilityObject.removeProperty(object, 'c');
+                const object = {a: 'A', b: 'B', c: 'C', d: 1, e: 2, f: 3},
+                    result = KarmiaUtilityObject.removeProperty(object, 'c');
+
                 expect(result).to.have.keys(['a', 'b', 'd', 'e', 'f']);
                 expect(result).to.not.have.key('c');
             });
+
             it('Should remove deep level property', function () {
                 const object = {
-                    a: {
-                        b: {
-                            c: {
-                                d: {
-                                    e: {
-                                        f: 'value'
+                        a: {
+                            b: {
+                                c: {
+                                    d: {
+                                        e: {
+                                            f: 'value'
+                                        }
                                     }
                                 }
                             }
                         }
-                    }
-                }, result = KarmiaUtilityObject.removeProperty(object, 'e');
+                    },
+                    result = KarmiaUtilityObject.removeProperty(object, 'e');
+
                 expect(result).to.eql({
                     a: {
                         b: {
@@ -188,23 +218,26 @@ describe('karmia-utility-object', function () {
                     }
                 });
             });
+
             it('Should remove multi level properties', function () {
                 const object = {
-                    a: {
                         a: {
-                            key: {}
-                        }
-                    },
-                    b: {
+                            a: {
+                                key: {}
+                            }
+                        },
                         b: {
                             b: {
                                 b: {
-                                    key: {}
+                                    b: {
+                                        key: {}
+                                    }
                                 }
                             }
                         }
-                    }
-                }, result = KarmiaUtilityObject.removeProperty(object, 'key');
+                    },
+                    result = KarmiaUtilityObject.removeProperty(object, 'key');
+
                 expect(result).to.eql({
                     a: {
                         a: {}
@@ -218,19 +251,22 @@ describe('karmia-utility-object', function () {
                     }
                 });
             });
+
             it('Should remove multi properties', function () {
                 const object = {
-                    a: {
                         a: {
-                            a1: {}
+                            a: {
+                                a1: {}
+                            }
+                        },
+                        b: {
+                            b: {
+                                b1: {}
+                            }
                         }
                     },
-                    b: {
-                        b: {
-                            b1: {}
-                        }
-                    }
-                }, result = KarmiaUtilityObject.removeProperty(object, ['a1', 'b1']);
+                    result = KarmiaUtilityObject.removeProperty(object, ['a1', 'b1']);
+
                 expect(result).to.eql({
                     a: {
                         a: {}
